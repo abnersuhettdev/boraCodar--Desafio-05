86579dd053e2143ec1369a12f665a1eeb421d9f5
Abner Suhett <99100863+abnersuhettdev@users.noreply.github.com>
2023-02-06 15:55:59 -0300

    começando calculadora

---
+++ script.js
@@ -0,0 +1,19 @@
+let currentValue = document.querySelector("#currentValue");
+let previousValue;
+let math;
+const divResult = document.querySelector("#resultado");
+
+let operator;
+
+buttons.addEventListener("click", (num) => {
+  num = Number(buttons.value);
+  console.log(num);
+});
+
+function getBtnValue(num) {}
+
+function init() {
+  document.querySelectorAll(".number").forEach((button) => {
+    button.addEventListener("click", function (e) {});
+  });
+}
